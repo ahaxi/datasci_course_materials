e369e2ad149b80524c421d8d529c86c4db10390e
ahaxi <yixifu@Yixis-MacBook-Air.local>
2014-07-22 22:10:35 -0700

    Added

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -7,11 +7,18 @@ def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+	# sent_file = open(sys.argv[1])
+	# tweet_file = open(sys.argv[2])
+	# hw()
+	# lines(sent_file)
+	# lines(tweet_file)
+	afinnfile = open("AFINN-111.txt")
+	scores = {}	# initialize an empty dictionary
+	for line in afinnfile:
+  		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+  		scores[term] = int(score)  # Convert the score to an integer.
+
+	print scores.items() # Print every (term, score) pair in the dictionary
 
 if __name__ == '__main__':
     main()
