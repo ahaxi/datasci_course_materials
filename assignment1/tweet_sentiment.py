932c285237f46d3997b0162565eb2931629c2be8
ahaxi <yixifu@Yixis-MacBook-Air.local>
2014-07-22 23:26:19 -0700

    Problem 2 Solved

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,4 +1,4 @@
-import sys
+import sys, json
 
 def hw():
     print 'Hello, world!'
@@ -12,13 +12,26 @@ def main():
 	# hw()
 	# lines(sent_file)
 	# lines(tweet_file)
-	afinnfile = open("AFINN-111.txt")
+	afinnfile = open(sys.argv[1])
 	scores = {}	# initialize an empty dictionary
 	for line in afinnfile:
-  		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+ 		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
   		scores[term] = int(score)  # Convert the score to an integer.
 
-	print scores.items() # Print every (term, score) pair in the dictionary
+	# print scores.items() # Print every (term, score) pair in the dictionary
+	# tweets = open("problem_1_submission.txt")
+	tweets = open(sys.argv[2])
+	for tweet in tweets:
+		tweetDict = json.loads(tweet)
+		if tweetDict.has_key(u'text'):
+			tscore = 0
+			contents = tweetDict['text'].strip().split()
+			for content in contents:
+				if scores.has_key(content):
+					tscore += scores[content]
+			print tscore				
+		else:
+			print str(0)
 
 if __name__ == '__main__':
     main()
