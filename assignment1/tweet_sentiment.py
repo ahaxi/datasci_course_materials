376e3afc55247d1de705cf4cb6adfd9137695638
ahaxi <yixifu@Yixis-MacBook-Air.local>
2014-08-12 22:18:53 -0700

    Change to #3

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -21,7 +21,9 @@ def main():
 	# print scores.items() # Print every (term, score) pair in the dictionary
 	# tweets = open("problem_1_submission.txt")
 	tweets = open(sys.argv[2])
+	unknownWords = {}
 	for tweet in tweets:
+		wbuffer = set()
 		tweetDict = json.loads(tweet)
 		if tweetDict.has_key(u'text'):
 			tscore = 0
@@ -29,9 +31,19 @@ def main():
 			for content in contents:
 				if scores.has_key(content):
 					tscore += scores[content]
-			print tscore				
-		else:
-			print str(0)
+				else:
+					wbuffer.add(content)
+			for w in wbuffer:
+				if unknownWords.has_key(w):
+					unknownWords[w][0] += tscore
+					unknownWords[w][1] += 1
+				else:
+					unknownWords.update({w:[tscore, 1]})
+#			print tscore				
+#		else:
+#			print str(0)
+	for u in unknownWords:
+		print u, float(round(unknownWords[u][0]/unknownWords[u][1], 2))
 
 if __name__ == '__main__':
     main()
